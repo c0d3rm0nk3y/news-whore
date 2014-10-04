718082d727ac78873a64e3050c6bc26c17734c7c
Peter <code4chaosmobile@gmail.com>
2014-10-04 12:50:18 -0500

    attempting to add user login.. can't test until on hardware

--- android/newswhore/app/src/main/java/monkey/datawhore/newswhore/rootActivity.java
+++ android/newswhore/app/src/main/java/monkey/datawhore/newswhore/rootActivity.java
@@ -1,5 +1,6 @@
 package monkey.datawhore.newswhore;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.CommonStatusCodes;
@@ -11,6 +12,10 @@ import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
@@ -36,30 +41,56 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import android.util.*;
+import android.widget.TextView;
 
 
 public class rootActivity extends Activity implements ConnectionCallbacks,
                                                       OnConnectionFailedListener  {
-  private static JSONObject jObj = null;
-  private boolean mSignInClicked;
-  private static final int RC_SIGN_IN = 0;
-  private GoogleApiClient mGoogleApiClient;
+  private static JSONObject   jObj = null;
+  private static final String SAVED_PROGRESS = "sign_in_progress";
+  private static final int    RC_SIGN_IN = 0;
+  private static final int    STATE_DEFAULT = 0;
+  private static final int    STATE_SIGN_IN = 1;
+  private static final int    STATE_IN_PROGRESS = 2;
+  private static final int    DIALOG_PLAY_SERVICE_ERROR = 0;
+
   private boolean mIntentInProgress;
+  private boolean mSignInClicked;
+
+  private int mSignInError;
+  private int mSignInProgress;
+
+  private PendingIntent mSignInIntent;
+  private GoogleApiClient mGoogleApiClient;
   private ConnectionResult mConnectionResult;
+
+
+  private TextView mTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_root);
 
-    mGoogleApiClient = new GoogleApiClient.Builder(this)
+    mTextView = (TextView) findViewById(R.id.textView01);
+    mTextView.setText("monkey hippy kitty puppy");
+    if(savedInstanceState != null) {
+      mSignInProgress = savedInstanceState.getInt(SAVED_PROGRESS, STATE_DEFAULT);
+    }
+
+    mGoogleApiClient = buildGoogleApiClient();
+
+  }
+
+  private GoogleApiClient buildGoogleApiClient() {
+    mTextView.setText("buildGoogleApiClient()..");
+    return new GoogleApiClient.Builder(this)
       .addConnectionCallbacks(this)
       .addOnConnectionFailedListener(this)
-      .addApi(Plus.API)
+      .addApi(Plus.API, Plus.PlusOptions.builder().build())
       .addScope(Plus.SCOPE_PLUS_LOGIN)
       .build();
   }
-
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,16 +113,45 @@ public class rootActivity extends Activity implements ConnectionCallbacks,
     case R.id.action_settings:
       openSettings();
       return true;
+      case R.id.getToken:
+        getAuthToken();
+      return true;
     case R.id.action_refresh:
       refreshContent();
       return true;
     case R.id.action_login:
-
+      // update status
+      mTextView.setText((CharSequence)"login taken..");
+      Log.i("MONKEY_TAG", getString(R.string.status_signing_in));
+      resolveSignInError();
       return true;
     default:
       return super.onOptionsItemSelected(item);
     }
   }
+
+  private void getAuthToken() {
+
+  }
+
+  private void resolveSignInError() {
+    mTextView.setText("resolveSignIn");
+    if(mSignInIntent != null) {
+      try {
+        mSignInProgress = STATE_IN_PROGRESS;
+        startIntentSenderForResult(mSignInIntent.getIntentSender(),
+                                   RC_SIGN_IN, null, 0, 0, 0);
+      } catch (IntentSender.SendIntentException e) {
+        Log.i("MONKEY_TAG", "Sign in intent could not be sent: " + e.getLocalizedMessage());
+        // The intent was canceled before it was sent.   attempt to connect to get an updated Connection Result
+        mSignInProgress = STATE_SIGN_IN;
+        mGoogleApiClient.connect();
+      }
+    } else {
+      showDialog(DIALOG_PLAY_SERVICE_ERROR);
+    }
+  }
+
 
   private static String url = "http://monkey-nodejs-71725.usw1.nitrousbox.com:8080/api/todaysnews?view=title+words+link&count=10";
 	private static String TAG = "MONKEYTAG";
@@ -193,12 +253,13 @@ public class rootActivity extends Activity implements ConnectionCallbacks,
 
   protected void onStart() {
     super.onStart();
+    mTextView.setText("onStart()..");
     mGoogleApiClient.connect();
   }
 
   protected void onStop() {
     super.onStop();
-
+    mTextView.setText("onStop()..");
     if(mGoogleApiClient.isConnected()) {
       mGoogleApiClient.disconnect();
     }
@@ -208,49 +269,141 @@ public class rootActivity extends Activity implements ConnectionCallbacks,
   public void onConnected(Bundle bundle) {
     // We've resolved any connection errors.  mGoogleApiClient can be used to
     // access Google APIs on behalf the user.
-
+    Log.i("MONKEY_TAG", "onConnected...");
+    mTextView.setText("onConnected..");
+    // update user interface.. blah blah blah
     /**
      *  get username..
      *  **/
-     Log.i("MONKEYTAG", "onConnected..");
      // update the ui to reflect..
 
     Person currentUser = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
 
-    Log.i("MONKEYTAG", "current user: " + currentUser.getDisplayName());
+    mTextView.setText(currentUser.getDisplayName());
+    Log.i("MONKEY_TAG", "current user: " + currentUser.getDisplayName());
 
+    mSignInProgress = STATE_DEFAULT;
 
   }
-
-
 
   @Override
   public void onConnectionFailed(ConnectionResult result) {
-    if(!mIntentInProgress && result.hasResolution()) {
-      try {
-        mIntentInProgress = true;
-        startIntentSenderForResult(result.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
-      } catch (IntentSender.SendIntentException e) {
-        // The intent was canceled before it was sent.  Return to the default
-        // state and attempt to connect to get an update ConnectionResult
-        mIntentInProgress = false;
-        mGoogleApiClient.connect();
+    // refer to the javadoc for ConnectionResult to see what
+    // error code might be returned in onConnectionFailed
+    Log.i("MONKEY_TAG", "conConnecttionFailed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    mTextView.setText("onConnectionFailed..");
+    if(mSignInProgress != STATE_IN_PROGRESS) {
+      // we do not have an intent in progress so we should store the latest
+      // error resolution intent to use when the sign in button is clicked
+      mSignInIntent = result.getResolution();
+      mSignInError = result.getErrorCode();
+      if(mSignInProgress == STATE_SIGN_IN) {
+        // STATE_SIGN_IN indicates the user already clicked the sign in button
+        // wo we should continue processing errors until the user is signed in or
+        // the clicked cancelled
+        resolveSignInError();
       }
     }
-  }
 
+    // in this sample we consider the user signed out whenever they do not have
+    // a connection to Google Play Services
+    onSignedOut();
 
-  public void onConnectionSuspended(int cause) {
-    mGoogleApiClient.connect();;
+//    if(!mIntentInProgress && result.hasResolution()) {
+//      try {
+//        mIntentInProgress = true;
+//        startIntentSenderForResult(result.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
+//      } catch (IntentSender.SendIntentException e) {
+//        // The intent was canceled before it was sent.  Return to the default
+//        // state and attempt to connect to get an update ConnectionResult
+//        mIntentInProgress = false;
+//        mGoogleApiClient.connect();
+//      }
+//    }
   }
 
   @Override
-  protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
-    if(requestCode == RC_SIGN_IN) {
-      mIntentInProgress = false;
-      if(!mGoogleApiClient.isConnecting()) {
-        mGoogleApiClient.connect();
-      }
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    mTextView.setText("onSaveInstanceState");
+    outState.putInt(SAVED_PROGRESS, mSignInProgress);
+  }
+
+  private void onSignedOut() {
+    // update the UI
+
+  }
+
+  // The connection to Google play Services was lost for some reason (hippies)
+  // We call connect to attempt to re-establish the connection or get a
+  // ConnectionResult that we can attempt to resolve
+  public void onConnectionSuspended(int cause) { mGoogleApiClient.connect(); }
+
+  @Override
+  protected Dialog onCreateDialog(int id) {
+    switch(id) {
+      case DIALOG_PLAY_SERVICE_ERROR:
+        if(GooglePlayServicesUtil.isUserRecoverableError(mSignInError)) {
+          return GooglePlayServicesUtil.getErrorDialog(
+            mSignInError,
+            this,
+            RC_SIGN_IN,
+            new DialogInterface.OnCancelListener() {
+              @Override
+              public void onCancel(DialogInterface dialog) {
+                Log.e("MONKEY_TAG", "Google Play Services resolution cancelled");
+                mSignInProgress = STATE_DEFAULT;
+                mTextView.setText(R.string.status_signed_out);
+              }
+            });
+
+        } else {
+          return new AlertDialog.Builder(this)
+            .setMessage(R.string.common_google_play_services_error)
+            .setPositiveButton("Close",
+              new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                  Log.e("MONKEY_TAG", "Google play services error could not be resolved: " + mSignInError);
+                  mSignInProgress = STATE_DEFAULT;
+                  mTextView.setText(R.string.status_signed_out);
+                }
+              }).create();
+
+        }
+        default:
+          return super.onCreateDialog(id);
     }
   }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    switch(requestCode) {
+      case RC_SIGN_IN:
+        if(resultCode == RESULT_OK) {
+          // if the error resoolution was successful we should continue processing errors.
+          mSignInProgress = STATE_SIGN_IN;
+        } else {
+          // if the error resolution was not successful or the user canceled,
+          // we should stop preocessing errors.
+          mSignInProgress = STATE_DEFAULT;
+        }
+
+        if(!mGoogleApiClient.isConnecting()) {
+          mGoogleApiClient.connect();
+        }
+      break;
+    }
+
+
+
+//    if(requestCode == RC_SIGN_IN) {
+//      mIntentInProgress = false;
+//      if(!mGoogleApiClient.isConnecting()) {
+//        mGoogleApiClient.connect();
+//      }
+//    }
+  }
+
+
 }
